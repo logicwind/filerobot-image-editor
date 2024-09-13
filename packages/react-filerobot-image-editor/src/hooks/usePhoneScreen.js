513000fd2d8fd572b174c0918613d64dd3937468
anurag-singh-us <anurag.singh@logicwind.com>
2024-09-13 12:33:12 +0530

    .

--- packages/react-filerobot-image-editor/src/hooks/usePhoneScreen.js
+++
@@ -1,4 +0,0 @@
-const usePhoneScreen = (screenWidth = 438) =>
-  window.matchMedia(`(max-width: ${screenWidth}px)`).matches;
-
-export default usePhoneScreen;
--- packages/react-filerobot-image-editor/src/utils/assignFinetuneNamesToKonva.js
+++
@@ -1,5 +0,0 @@
-const assignFinetuneNamesToKonva = () => {
-  Object.keys(Konva.Filters).forEach((key) => Konva.Filters[key].finetuneName = key)
-}
-
-export default assignFinetuneNamesToKonva;
